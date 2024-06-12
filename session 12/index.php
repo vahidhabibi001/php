29617ada3c2d329ed155656abd4b3084ba9af63b
vahid <168022480+vahidhabibi001@users.noreply.github.com>
2024-06-12 21:54:11 +0330

    start php //

---
+++ session 12/index.php
@@ -0,0 +1,32 @@
+<?php 
+    //  str
+$name = "vahid" ;
+$last_name = "habibi" ;
+   //  int
+$age = 19 ;
+    // float
+$score = 18.5 ;
+$lscore = 15.6 ;
+    //  bool
+$male = true ;
+
+echo $name ;
+echo "</br>";
+echo $last_name ;
+echo "</br>" ;
+echo $age ;
+$x = 55;
+$y = 12;
+echo "</br>" ;
+ echo "$x + $y = " ;
+ echo $x + $y  ;
+ echo "</br>" ;
+ echo "$x - $y =" ;
+ echo $x - $y;
+ echo '</br>' ;
+ echo "$x * $y = " ;
+ echo $x*$y ;
+ echo "</br>" ; 
+ echo "$score % $lscore = ";
+ echo $score % $lscore ;
+ echo $male ;
